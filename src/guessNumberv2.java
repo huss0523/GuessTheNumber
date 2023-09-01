5dda2c0e79c5384f98867aed00d7c8513c074639
hussain <hussainomar5@gmail.com>
2023-09-01 14:53:39 +0200

    actual working version 2.0 - alot of fixes and changes. very good version

--- src/guessNumberv2.java
+++ src/guessNumberv2.java
@@ -23,18 +23,31 @@ public class guessNumberv2 {
 
             if (secretNumber == userGuess) {
                 System.out.println("Congratulations! You guessed correctly! Well done.");
-                System.out.printf("You guessed correctly with %d attempt(s) remaining!",remainingUserGuess);
-                //Want to update attempts to say attempt when 1 attempt remaining
+                String strRemaining = (remainingUserGuess == 1) ? "attempt" :"attempts";
+                // Attempts need to be able to change from attempts to attempt *FIXED*
+                System.out.printf("You guessed correctly with %d %s remaining!",remainingUserGuess,strRemaining);
             } else if (secretNumber > userGuess) {
-                System.out.printf("Incorrect. You have %d tries remaining...\n", maxUserGuess);
-                System.out.println("The number I'm thinking of is higher...");
+                String strTries = (remainingUserGuess == 1) ? "try" :"tries";
+                // Tries need to be able to change from Tries to 1 try *FIXED*
+                System.out.printf("Incorrect. You have %d %s remaining",maxUserGuess,strTries);
+                if (remainingUserGuess >0) //FIXED HINT AT 0 GUESSES
+                    System.out.println("\nThe number I'm thinking of is higher...");
             } else {
-                System.out.printf("Incorrect. You have %d tries remaining...\n", maxUserGuess);
-                System.out.println("The number I'm thinking of is lower...");
+                String strTries = (remainingUserGuess == 1) ? "try" :"tries";
+                // Tries need to be able to change from Tries to 1 try *FIXED*
+                System.out.printf("Incorrect. You have %d %s remaining",maxUserGuess,strTries);
+                    if (remainingUserGuess >0) //FIXED HINT AT 0 GUESSES
+                        System.out.println("\nThe number I'm thinking of is lower...");
             }
 
-            if (maxUserGuess == 0)
+            if (maxUserGuess == 0 && secretNumber!=userGuess)
+                //Need to fix so it dosent print msg below at 0 guesses *FIXED WITH &&secretNumber!=userGuess*
                 System.out.printf("%nThanks for playing! %nThe number i was thinking of was %d", secretNumber);
+            // ^^Code needs to be fixed, still displays number at the 0 attempts. *FIXED*
+
+            //Code still generates hint at failure(0 guesses). Needs to be changed so it dosent print at last attempt
+            //FIXED HINT AT 0 GUESSES with
+
         }
     }
 }
